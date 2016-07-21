1263364970ac5e76ec21dfe27f599be353161d3b
Manu Uzkudun <uzkudun@mdpi.com>
2016-07-21 15:53:39 +0200

    Small journal list addedc in the homepage

--- src/MDPI/MDPIEsBundle/Controller/HomePageController.php
+++ src/MDPI/MDPIEsBundle/Controller/HomePageController.php
@@ -3,11 +3,19 @@
 namespace MDPI\MDPIEsBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use MDPI\AppBundle\Entity\JournalRepository;
 
 class HomePageController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('MDPIEsBundle:Home:index.html.twig');
+    	
+		$journals = JournalRepository::getAllPublicJournals();
+		$journals = array_slice($journals, 0, 12);
+		
+        return $this->render('MDPIEsBundle:Home:index.html.twig',array(
+			
+		'journals' => $journals,
+		));
     }
 }
