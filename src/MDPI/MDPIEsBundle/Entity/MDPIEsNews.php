41fab113cdf84bcd63bf93feee069c52a0bec1fe
Manu Uzkudun <uzkudun@mdpi.com>
2016-08-17 10:09:57 +0200

    News entity added

---
+++ src/MDPI/MDPIEsBundle/Entity/MDPIEsNews.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace MDPI\MDPIEsBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+* @ORM\Entity
+* @ORM\Table(name="mdpiesnews")
+*/
+class MDPIEsNews
+{
+	/**
+	* @ORM\Column(type="integer")
+	* @ORM\Id
+	* @ORM\GeneratedValue(strategy="AUTO")
+	*/
+	private $id;
+	
+	/**
+	* @ORM\Column(type="string")
+	*/
+	private $title;
+	
+	/**
+	* @ORM\Column(type="string",length= 50)
+	*/
+	private $slug;
+	
+	/**
+	* @ORM\Column(type="datetime")
+	*/
+	private $date;
+	
+	/**
+	* @ORM\Column(type="text")
+	*/
+	private $content;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return MDPIEsNews
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string 
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set slug
+     *
+     * @param string $slug
+     * @return MDPIEsNews
+     */
+    public function setSlug($slug)
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    /**
+     * Get slug
+     *
+     * @return string 
+     */
+    public function getSlug()
+    {
+        return $this->slug;
+    }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     * @return MDPIEsNews
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime 
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set content
+     *
+     * @param string $content
+     * @return MDPIEsNews
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * Get content
+     *
+     * @return string 
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+}
