eaaa4cd3d5e00f122fe3b20d69adec9fb61cbdf3
Manu Uzkudun <uzkudun@mdpi.com>
2016-09-01 17:37:28 +0200

    Installs susabundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new MDPI\MDPIEsBundle\MDPIEsBundle(),
         	new MDPI\AppBundle\MDPIAppBundle(),
+        	new MDPI\SudaBundle\MDPISudaBundle(),
             
             // GFinder
             new GFinder\GFinderBundle($this),		
